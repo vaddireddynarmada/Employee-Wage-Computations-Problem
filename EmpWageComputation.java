661c0d6bb7b070811a5c85bc0b0a59151711f407
unknown <narmada.vaddireddy97@gmail.com>
2023-02-23 11:34:26 +0530

    Part time Employee & Wage for Part time Hour is 8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,13 +1,18 @@
 public class EmpWageComputation {
-    public static void main(String[] args) {
-        int present = 1;
+    public static void main(String []args) {
+        int fullTime = 1;
+        int partTime = 2;
         int wagePerHour = 20;
         int workingHour = 0;
-        int attendance = (int) (Math.random() * 100) % 2;
+        int attendance = (int) (Math.random() * 100) % 3;
 
-        if (present == attendance) {
-            System.out.println("employee is present");
+        if (fullTime == attendance) {
+            System.out.println("employee worked for full time");
             workingHour = 8;
+
+        } else if (partTime == attendance) {
+            System.out.println("employee worked for part time");
+            workingHour = 4;
 
         } else {
             System.out.println("employee is absent");
@@ -15,6 +20,5 @@ public class EmpWageComputation {
 
         int wage = wagePerHour * workingHour;
         System.out.println("Employee daily wage: " + wage + " Rupees.");
-
     }
 }
