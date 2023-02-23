52a110a576dc8b345e755fcdbd7f0622a19e3526
unknown <narmada.vaddireddy97@gmail.com>
2023-02-23 11:29:35 +0530

    Daily Employee WageEmployee Wage - Assume that Wage per Hour is 20 and Full Day Hour is 8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,13 +1,20 @@
 public class EmpWageComputation {
     public static void main(String[] args) {
         int present = 1;
-        int attendance = (int) (Math.random()*100) % 2;
-        if (present == attendance) {
-            System.out.println("Employee is present");
-        }
-        else {
-            System.out.println("Employee is present");
+        int wagePerHour = 20;
+        int workingHour = 0;
+        int attendance = (int) (Math.random() * 100) % 2;
 
+        if (present == attendance) {
+            System.out.println("employee is present");
+            workingHour = 8;
+
+        } else {
+            System.out.println("employee is absent");
         }
+
+        int wage = wagePerHour * workingHour;
+        System.out.println("Employee daily wage: " + wage + " Rupees.");
+
     }
 }
