005903fb90ce6dc13c95a6cd78d6166930082acd
unknown <narmada.vaddireddy97@gmail.com>
2023-02-23 11:23:23 +0530

    Employee is Present or Absent by using random

---
+++ EmpWageComputation.java
@@ -0,0 +1,13 @@
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        int present = 1;
+        int attendance = (int) (Math.random()*100) % 2;
+        if (present == attendance) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is present");
+
+        }
+    }
+}
